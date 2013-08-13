13f26d82d38e08c7866e27a0c7750dfa964b498c
Colin Bankier <colinbankier@gmail.com>
2013-08-13 23:17:01 +1000

    Add redirect to hamepage for page not found.

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
-describe ProjectsController do
-
+describe ProjectsController, :type => :controller do
+  it "should redirect to homepage if page not found" do
+    get :show, :id => "not-here"
+    response.should redirect_to(projects_path)
+    message = "The project you were looking for could not be found."
+    flash[:alert].should eql(message)
+  end
 end
